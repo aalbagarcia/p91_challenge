0da4108abb8ced1cce55cad69e341482143c8e6e
Alfonso Alba García <cursos@cursodegit.com>
2020-03-30 21:08:13 +0200

    Add bootstrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery/dist/jquery.js')
+require('bootstrap/dist/js/bootstrap.js')
+require('bootstrap/dist/css/bootstrap.css')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
